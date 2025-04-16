d0c2d99e80e39f9906c50071281d1335eff200cc
Wagner Victor <wagnervicwv@gmail.com>
2025-04-16 10:18:04 -0300

    feat(init): start SPFx WebPart PedidosMembership

---
+++ src/webparts/pedidosMembership/components/IPedidosMembershipProps.ts
@@ -0,0 +1,7 @@
+export interface IPedidosMembershipProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
